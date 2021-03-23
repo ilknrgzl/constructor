8e138602468955b8b1e966e063d00122bac8063a
İlknur Güzel <ilknrgzl99@gmail.com>
2021-03-23 10:15:46 +0300

    güncellendi

--- ConsoleApp38/Program.cs
+++ ConsoleApp38/Program.cs
@@ -4,18 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp38
+namespace Constructor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Customer customer = new Customer { Id = 1, FirstName = "ilknur", LastName = "güzel", City = "ankara" };
+            Customer customer2 = new Customer ( 2, "İrem", "Güzel", "ankara" );
+            
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+
+
         }
+        
     }
+
+
+    class Customer
+    {   // default constructor
+        public  Customer(int ıd, string firstName,string lastName,string city)  //parametre ekleyerek 14 satırdaki kodu çalışır hale getirdik ancak bu şekilde 13 satır çalışır durumda değil
+        {
+            Console.WriteLine("Yapıcı blok çalıştı");
+
+        }
+        public Customer() //bu method ile overloading ederek kodu ezdik
+        {
+
+        }
+
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+
 }

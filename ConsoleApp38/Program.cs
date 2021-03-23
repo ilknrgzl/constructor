72be7d2248fbaf346bfda811493e3bcdb9a79bbb
İlknur Güzel <ilknrgzl99@gmail.com>
2021-03-23 13:31:41 +0300

    yeni eklemeler yapıldı ve güncelleştirildi.

--- ConsoleApp38/Program.cs
+++ ConsoleApp38/Program.cs
@@ -11,11 +11,17 @@ namespace Constructor
         static void Main(string[] args)
         {
             Customer customer = new Customer { Id = 1, FirstName = "ilknur", LastName = "güzel", City = "ankara" };
+
             Customer customer2 = new Customer ( 2, "İrem", "Güzel", "ankara" );
-            
+
+            Customer customer3 = new Customer();
+            customer3.Id = 2;
 
 
+            Console.WriteLine(customer2.FirstName);
 
+
+            Console.ReadLine();
 
         }
         
@@ -26,14 +32,21 @@ namespace Constructor
     {   // default constructor
         public  Customer(int ıd, string firstName,string lastName,string city)  //parametre ekleyerek 14 satırdaki kodu çalışır hale getirdik ancak bu şekilde 13 satır çalışır durumda değil
         {
+            Id = Id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+
             Console.WriteLine("Yapıcı blok çalıştı");
 
         }
+        
+
         public Customer() //bu method ile overloading ederek kodu ezdik
         {
 
-        }
 
+        }
 
         public int Id { get; set; }
         public string FirstName { get; set; }
